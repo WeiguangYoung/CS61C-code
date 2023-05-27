4c7b7db4a8273876d54549f386f59a15e050ef05
yangweiguang <727554434@qq.com>
2023-05-27 10:09:20 +0800

    feat: lab02 lfsr

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -4,7 +4,16 @@
 #include <string.h>
 #include "lfsr.h"
 
-void lfsr_calculate(uint16_t *reg) {
+void lfsr_calculate(uint16_t *reg)
+{
     /* YOUR CODE HERE */
-}
+    int head = 0;
+    head ^= (((*reg) >> 0) & 1);
+    head ^= (((*reg) >> 2) & 1);
+    head ^= (((*reg) >> 3) & 1);
+    head ^= (((*reg) >> 5) & 1);
 
+    *reg >>= 1;
+    *reg = *reg | (head << 15);
+
+}
