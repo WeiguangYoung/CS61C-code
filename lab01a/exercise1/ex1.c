1ca15e9f13faf13ef0500a3d1fd1d34ac2f0134f
yangweiguang <727554434@qq.com>
2023-06-08 16:48:33 +0800

    add lab01a ex1

--- lab01a/exercise1/ex1.c
+++ lab01a/exercise1/ex1.c
@@ -5,15 +5,73 @@
 There are two different ways to iterate through a string.
 1st way hint: strlen() may be useful
 2nd way hint: all strings end in a null terminator */
-int num_occurrences(char *str, char letter) {
+
+int num_occurrences(char *str, char letter)
+{
     /* TODO: implement num_occurances */
-    return 0;
+    int num = 0;
+    int len = strlen(str);
+
+    for (int i = 0; i < len; i++)
+    {
+        if (str[i] == letter)
+        {
+            num++;
+        }
+    }
+    return num;
 }
+
+// int num_occurrences(char *str, char letter)
+// {
+//     /* TODO: implement num_occurances */
+//     int num = 0;
+//     int i = 0;
+
+//     while (*(str + i) != '\0')
+//     {
+//         if (*(str + i) == letter)
+//         {
+//             num++;
+//         }
+//         i++;
+//     }
+//     return num;
+// }
 
 /* Populates DNA_SEQ with the number of times each nucleotide appears.
 Each sequence will end with a NULL terminator and will have up to 20 nucleotides.
 All letters will be upper case. */
-void compute_nucleotide_occurrences(DNA_sequence *dna_seq) {
+void compute_nucleotide_occurrences(DNA_sequence *dna_seq)
+{
     /* TODO: implement compute_nucleotide_occurances */
+
+    int len = strlen(dna_seq->sequence);
+    dna_seq->A_count = 0;
+    dna_seq->C_count = 0;
+    dna_seq->G_count = 0;
+    dna_seq->T_count = 0;
+
+    for (int i = 0; i < len; i++)
+    {
+
+        if (dna_seq->sequence[i] == 'A')
+        {
+            dna_seq->A_count++;
+        }
+        else if (dna_seq->sequence[i] == 'C')
+        {
+            dna_seq->C_count++;
+        }
+        else if (dna_seq->sequence[i] == 'G')
+        {
+            dna_seq->G_count++;
+        }
+        else if (dna_seq->sequence[i] == 'T')
+        {
+            dna_seq->T_count++;
+        }
+    }
+
     return;
 }
