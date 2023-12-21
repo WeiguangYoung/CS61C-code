b22c0912eb6682f34a7a2fd68b1116c2d3296f02
yangweiguang <727554434@qq.com>
2023-12-21 11:34:56 +0800

    finish proj2 argmax

--- proj2/src/argmax.s
+++ proj2/src/argmax.s
@@ -8,6 +8,12 @@
 # Arguments:
 #   a0 (int*) is the pointer to the start of the array
 #   a1 (int)  is the # of elements in the array
+# Registers:
+#   t0, index of array
+#   t1, memory location of an specific element
+#   t2, specific element
+#   t3, maximum element index
+#   t4, maximum element
 # Returns:
 #   a0 (int)  is the first index of the largest element
 # Exceptions:
@@ -16,15 +22,37 @@
 # =================================================================
 argmax:
 	# Prologue
-
+	li t0, 1
+	blt a1, t0, exception
+	mv t0, x0
+	mv t3, x0
 
 loop_start:
+	slli t1, t0, 2
+	add t1, a0, t1
+	lw t2, 0(t1)
+	beq t0, x0, cache_first
+	bgt t2, t4, update_index
+	j loop_continue
 
+cache_first:
+    lw t4, 0(t1)
+	j loop_continue
+
+update_index:
+	mv t3, t0
+	mv t4, t2
 
 loop_continue:
-
+	addi t0, t0, 1
+	blt t0, a1, loop_start
+	j loop_end
 
 loop_end:
 	# Epilogue
-
+	mv a0, t3
 	ret
+
+exception:
+    li a0, 36
+    j exit
